fac9a2ed7fecf62e3de2401ac8b260bbd703b1a3
samiraha1 <samiraha@email.sc.edu>
2025-10-15 18:25:54 -0400

    react

---
+++ react-01/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return <h1>Hello World</h1>;
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  // );
+}
+
+export default App;
