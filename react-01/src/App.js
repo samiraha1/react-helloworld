39f2637cf2adc5744f602af50ecffda6726ac577
samiraha1 <samiraha@email.sc.edu>
2025-10-16 12:42:26 -0400

    message

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 
 function App() {
-  return <h1>Hello World</h1>;
-    // <div className="App">
-    //   <header className="App-header">
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Have a nice day</p>
+    </div>);
+  // <div className="App">
+  //   <header className="App-header">
+  //     <p>
+  //       Edit <code>src/App.js</code> and save to reload.
+  //     </p>
+  //     <a
+  //       className="App-link"
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Learn React
+  //     </a>
+  //   </header>
+  // </div>
   // );
 }
 
